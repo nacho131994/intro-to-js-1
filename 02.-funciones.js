043528fa9198d51e5f58a2bb4bb9fbc6a1267479
agarzon48 <garzonximenez@gmail.com>
2023-01-25 16:07:41 +0000

    Add intro, HTML imports and variable docs

---
+++ 02.-funciones.js
@@ -0,0 +1,65 @@
+// LAS FUNCIONES SON CONTENEDORES DE VALORES
+// 1. Debemos definirlas para poder utilizarlas.
+// 2. A continuación, podemos asignarles un valor
+
+// var example;
+// example = 'Hello, world!';
+
+// IMPORTANTE:
+// DEBEMOS DEFINIR Y ASIGNAR VALOR A LA VEZ:
+
+// var example = 'Hello, world!';
+
+// IMPORTANTE:
+// NO DEBEMOS UTILIZAR var, SINO:
+// 1. const: siempre que podamos, ya que impide que luego le cambiemos el valor por accidente
+// 2. let: cuando necesitemos reasignar valor más adelante en nuestro código
+
+// let example = 'Hello, world!';
+// const secondExample = 'Hello, world!';
+
+
+// ¿QUÉ DIFERENCIAS HAY ENTRE var, let Y const?
+
+// var es una forma antigua de declarar variables. En general, no la utilizamos.
+// Actúa en el global scope y permite la reasignación de valores.
+// El hoisting permite que las utilicemos en cualquier lugar del documento.
+
+// let y const son la nueva forma de declarar variables. Utilizamos const siempre que podamos,
+// y let cuando tengamos que reasignar valor posteriormente. Ambas actúan en local scope
+// No se someten a hoisting, por lo que solo se pueden utilizar una vez declaradas
+
+
+// ---
+// Ejemplo de hoisting:
+// La declaración de la variable sube al principio del archivo. No su inicialización
+
+// console.log(hoistedHello);
+
+// var hoistedHello = 'Hello, world!'; // undefined
+
+// console.log(notHoistedHello);
+
+// let notHoistedHello = 'Hello, world'; // ReferenceError
+
+
+// ---
+// Ejemplo de reasignación:
+
+// var varReassignExample = 'Hello, world!';
+// console.log(varReassignExample); // 'Hello, world!'
+
+// let letReassignExample = 'Hello, world!';
+// console.log(letReassignExample); // 'Hello, world!'
+
+// const constReassignExample = 'Hello, world!';
+// console.log(constReassignExample); // 'Hello, world!'
+
+// varReassignExample = 'Goodbye, world!';
+// console.log(varReassignExample); // 'Goodbye, world!'
+
+// letReassignExample = 'Goodbye, world!';
+// console.log(letReassignExample); // 'Goodbye, world!'
+
+// constReassignExample = 'Goodbye, world!'; // TypeError
+
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# intro-to-js
+# Apuntes básicos de JavaScript
+
+En este repositorio iremos anotando los conceptos más básicos de JavaScript y haremos algunas prácticas.
+
+* Cada concepto o ejercicio irá en su propio archivo. Para ejecutar es necesario tener node instalado y lanzar el siguiente comando:
+
+````
+node run <nombre del archivo>
+````
